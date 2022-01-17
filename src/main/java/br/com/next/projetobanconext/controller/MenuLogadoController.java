aecf47c236e56f08177badcb440c74d4c962c43b
Sysorkole <arthurfigon@gmail.com>
2022-01-17 01:37:37 -0300

    Mais alterações visuais e alteração no gerador de número para forçar a sempre gerar números de 4 digitos...

--- src/main/java/br/com/next/projetobanconext/controller/MenuLogadoController.java
+++ src/main/java/br/com/next/projetobanconext/controller/MenuLogadoController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -125,5 +126,9 @@ public class MenuLogadoController implements Initializable {
         listConta = BancoDeDados.returnTodasContas();
         observableList = FXCollections.observableList(listConta);
         contaComboBox.setItems(observableList);
+    }
+
+    @FXML
+    public void onBtAreaPixAction() {
     }
 }
